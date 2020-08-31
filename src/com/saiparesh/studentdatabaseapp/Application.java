c8dcbf8d5abc4f80daec5e2805e74c137701fea4
Paresh <sai.paresh@capgemini.com>
2020-08-31 12:19:57 +0530

    First commit created class outline

---
+++ src/com/saiparesh/studentdatabaseapp/Application.java
@@ -0,0 +1,11 @@
+package com.saiparesh.studentdatabaseapp;
+
+public class Application {
+
+	public static void main(String[] args) {
+		//Ask how many users will be 
+		
+		//Create n number of students
+	}
+
+}
---
+++ src/com/saiparesh/studentdatabaseapp/Student.java
@@ -0,0 +1,22 @@
+package com.saiparesh.studentdatabaseapp;
+
+public class Student {
+	private String firstname;
+	private String lastName;
+	private String gradeYear;
+	private int studentId;
+	private String courses;
+	private int tuitionBalance;
+	private int costOfCourse = 600;
+	//constructor: prompt user to enter student's name and year
+	
+	// Generate an id
+	
+	// Enroll in courses
+	
+	// View balance 
+	
+	// Pay tuition
+	
+	// Show status
+}
