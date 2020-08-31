f442963fe1da3a369dedb2e3cb711df4e2ee0b4b
Paresh <sai.paresh@capgemini.com>
2020-08-31 15:50:14 +0530

    Student databse app

--- src/com/saiparesh/studentdatabaseapp/Application.java
+++ src/com/saiparesh/studentdatabaseapp/Application.java
@@ -1,11 +1,32 @@
 package com.saiparesh.studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
-		//Ask how many users will be 
+		//Ask how many students will be added
+		
+		System.out.println("Enter number of students to be involved : ");
+		Scanner in = new Scanner(System.in);
 		
 		//Create n number of students
+		
+		int numberOfStudents = in.nextInt();
+		Student[] students = new Student[numberOfStudents];
+		
+		for (int i = 0; i < numberOfStudents; i++) {
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].payTuition();
+			students[i].showInfo();
+		}
+		
+//		Student s1 = new Student();
+//		s1.enroll();
+//		s1.payTuition();
+//		s1.showInfo();
+		
 	}
 
 }
--- src/com/saiparesh/studentdatabaseapp/Student.java
+++ src/com/saiparesh/studentdatabaseapp/Student.java
@@ -1,22 +1,83 @@
 package com.saiparesh.studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 	private String firstname;
 	private String lastName;
-	private String gradeYear;
-	private int studentId;
-	private String courses;
-	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private int gradeYear;
+	private String studentId;
+	private String courses = "";
+	private int tuitionBalance = 0;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+	
 	//constructor: prompt user to enter student's name and year
 	
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter Student's First name : ");
+		this.firstname = in.nextLine();
+		
+		System.out.print("Enter Student's Last name : ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior \n4 Senior\nEnter class level : ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentId();
+		
+		
+	}
+	
 	// Generate an id
+	private void setStudentId() {
+		// Grade level + static id		
+		this.studentId =  gradeYear + "" + id; 
+		id++;
+	}
 	
 	// Enroll in courses
-	
+	public void enroll() {
+		// Get inside loop, user hits zero
+		
+		do {			
+			System.out.print("Enter course to enroll (Q/q to quit) : ");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if(!course.equals("Q") && !course.equals("q")) {
+				courses = courses + "\n   " + course;
+				tuitionBalance += costOfCourse;
+			}
+			else {
+				break;
+			}
+			
+		} while (1 != 0);
+
+	}
 	// View balance 
+	public void viewBalance() {
+		System.out.println("Your balance is : " + tuitionBalance);
+	}
 	
 	// Pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter amount you want to pay : ");
+		Scanner in = new Scanner(System.in);
+		int amount = in.nextInt();
+		tuitionBalance -= amount;
+		System.out.println("Thank you for your payment of : " + amount);
+		viewBalance();
+	}
 	
 	// Show status
+	public void showInfo() {
+		System.out.println("Name: " + firstname + lastName + 
+				"\nGrade Level: " +  gradeYear +
+				"\nStudent ID : " + studentId +
+				"\nCourses: " + courses +
+				"\nBalance: "+ tuitionBalance);
+	}
 }
